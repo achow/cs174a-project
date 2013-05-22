878036c84e423f6ffcfbccd6fd22588ad874af76
Amy Lin <amy_y_lin@outlook.com>
2013-05-21 20:59:19 -0700

    webgl for cube and sphere

--- src/cube.buffer.js
+++ src/cube.buffer.js
@@ -1,1 +1,175 @@
+CubeBuffer = new JS.Class({
 
+	 initialize: function() {
+		this.initCube();
+		this.positionBuffer;
+	 },
+
+	 initCube: function() {
+		this.numTriangles = 12;
+        this.numVertices = 3*this.numTriangles;
+        this.Vertices = [];
+        this.Normals = [];
+
+        //this.Index = 0;
+		
+		this.VIndex = 0;
+		this.NIndex = 0;
+		
+        this.generateCube();
+
+
+     },
+
+	quad: function(a, b, c, d)
+	{
+		var oneSet = [];
+		
+			oneSet[0] = [ -0.5, -0.5,  0.5, 1.0 ];
+			oneSet[1] = [ -0.5,  0.5,  0.5, 1.0 ];
+			oneSet[2] = [ 0.5,  0.5,  0.5, 1.0 ];
+			oneSet[3] = [ 0.5, -0.5,  0.5, 1.0 ];
+			oneSet[4] = [ -0.5, -0.5, -0.5, 1.0 ];
+			oneSet[5] = [ -0.5,  0.5, -0.5, 1.0 ];
+			oneSet[6] = [ 0.5,  0.5, -0.5, 1.0 ];
+			oneSet[7] = [ 0.5, -0.5, -0.5, 1.0 ];
+
+		this.Vertices[this.VIndex] = oneSet[a][0];
+		this.VIndex++;	
+		this.Vertices[this.VIndex] = oneSet[a][1];
+		this.VIndex++;	
+		this.Vertices[this.VIndex] = oneSet[a][2];
+		this.VIndex++;	
+		this.Vertices[this.VIndex] = oneSet[a][3];
+		this.VIndex++;		
+        this.Normals[this.NIndex] = oneSet[a][0];
+		this.NIndex++;
+        this.Normals[this.NIndex] = oneSet[a][1];
+		this.NIndex++;
+        this.Normals[this.NIndex] = oneSet[a][2];
+		this.NIndex++;
+
+		this.Vertices[this.VIndex] = oneSet[b][0];
+		this.VIndex++;		
+		this.Vertices[this.VIndex] = oneSet[b][1];
+		this.VIndex++;	
+		this.Vertices[this.VIndex] = oneSet[b][2];
+		this.VIndex++;	
+		this.Vertices[this.VIndex] = oneSet[b][3];
+		this.VIndex++;		
+        this.Normals[this.NIndex] = oneSet[b][0];
+		this.NIndex++;
+        this.Normals[this.NIndex] = oneSet[b][1];
+		this.NIndex++;
+        this.Normals[this.NIndex] = oneSet[b][2];
+		this.NIndex++;		
+
+		this.Vertices[this.VIndex] = oneSet[c][0];
+		this.VIndex++;		
+		this.Vertices[this.VIndex] = oneSet[c][1];
+		this.VIndex++;	
+		this.Vertices[this.VIndex] = oneSet[c][2];
+		this.VIndex++;	
+		this.Vertices[this.VIndex] = oneSet[c][3];
+		this.VIndex++;		
+        this.Normals[this.NIndex] = oneSet[c][0];
+		this.NIndex++;
+        this.Normals[this.NIndex] = oneSet[c][1];
+		this.NIndex++;
+        this.Normals[this.NIndex] = oneSet[c][2];
+		this.NIndex++;
+		
+		this.Vertices[this.VIndex] = oneSet[a][0];
+		this.VIndex++;		
+		this.Vertices[this.VIndex] = oneSet[a][1];
+		this.VIndex++;	
+		this.Vertices[this.VIndex] = oneSet[a][2];
+		this.VIndex++;	
+		this.Vertices[this.VIndex] = oneSet[a][3];
+		this.VIndex++;		
+        this.Normals[this.NIndex] = oneSet[a][0];
+		this.NIndex++;
+        this.Normals[this.NIndex] = oneSet[a][1];
+		this.NIndex++;
+        this.Normals[this.NIndex] = oneSet[a][2];
+		this.NIndex++;
+
+		this.Vertices[this.VIndex] = oneSet[c][0];
+		this.VIndex++;		
+		this.Vertices[this.VIndex] = oneSet[c][1];
+		this.VIndex++;	
+		this.Vertices[this.VIndex] = oneSet[c][2];
+		this.VIndex++;	
+		this.Vertices[this.VIndex] = oneSet[c][3];
+		this.VIndex++;		
+        this.Normals[this.NIndex] = oneSet[c][0];
+		this.NIndex++;
+        this.Normals[this.NIndex] = oneSet[c][1];
+		this.NIndex++;
+        this.Normals[this.NIndex] = oneSet[c][2];
+		this.NIndex++;
+		
+		this.Vertices[this.VIndex] = oneSet[d][0];
+		this.VIndex++;		
+		this.Vertices[this.VIndex] = oneSet[d][1];
+		this.VIndex++;	
+		this.Vertices[this.VIndex] = oneSet[d][2];
+		this.VIndex++;	
+		this.Vertices[this.VIndex] = oneSet[d][3];
+		this.VIndex++;		
+        this.Normals[this.NIndex] = oneSet[d][0];
+		this.NIndex++;
+        this.Normals[this.NIndex] = oneSet[d][1];
+		this.NIndex++;
+        this.Normals[this.NIndex] = oneSet[d][2];
+		this.NIndex++;
+		
+		/*
+			oneSet[0] = vec4.fromValues( -0.5, -0.5,  0.5, 1.0 );
+			oneSet[1] = vec4.fromValues( -0.5,  0.5,  0.5, 1.0 );
+			oneSet[2] = vec4.fromValues( 0.5,  0.5,  0.5, 1.0 );
+			oneSet[3] = vec4.fromValues( 0.5, -0.5,  0.5, 1.0 );
+			oneSet[4] = vec4.fromValues( -0.5, -0.5, -0.5, 1.0 );
+			oneSet[5] = vec4.fromValues( -0.5,  0.5, -0.5, 1.0 );
+			oneSet[6] = vec4.fromValues( 0.5,  0.5, -0.5, 1.0 );
+			oneSet[7] = vec4.fromValues( 0.5, -0.5, -0.5, 1.0 );
+			
+		this.Vertices[this.Index] = oneSet[a];
+        this.Normals[this.Index] = vec3.fromValues(oneSet[a][0], oneSet[a][1], oneSet[a][2]);
+		this.Index++;
+		
+		this.Vertices[this.Index] = oneSet[b];
+        this.Normals[this.Index] = vec3.fromValues(oneSet[b][0], oneSet[b][1], oneSet[b][2]);
+		this.Index++;
+
+		this.Vertices[this.Index] = oneSet[c];
+        this.Normals[this.Index] = vec3.fromValues(oneSet[c][0], oneSet[c][1], oneSet[c][2]);
+		this.Index++;
+		
+		this.Vertices[this.Index] = oneSet[a];
+        this.Normals[this.Index] = vec3.fromValues(oneSet[a][0], oneSet[a][1], oneSet[a][2]);
+		this.Index++;
+		
+		this.Vertices[this.Index] = oneSet[c];
+        this.Normals[this.Index] = vec3.fromValues(oneSet[c][0], oneSet[c][1], oneSet[c][2]);
+		this.Index++;
+		
+		this.Vertices[this.Index] = oneSet[d];
+        this.Normals[this.Index] = vec3.fromValues(oneSet[d][0], oneSet[d][1], oneSet[d][2]);
+		this.Index++;
+			*/
+		
+
+	},
+
+	generateCube: function()
+	{
+		this.quad( 1, 0, 3, 2 );
+		this.quad( 2, 3, 7, 6 );
+		this.quad( 3, 0, 4, 7 );
+		this.quad( 6, 5, 1, 2 );
+		this.quad( 4, 5, 6, 7 );
+		this.quad( 5, 4, 0, 1 );
+	}
+
+});
--- src/sphere.buffer.js
+++ src/sphere.buffer.js
@@ -1,1 +1,127 @@
+SphereBuffer = new JS.Class({
 
+	 initialize: function() {
+		this.initSphere();
+		this.positionBuffer;
+	 },
+
+	 initSphere: function() {
+        this.numVertices = (Math.pow(4, 5+1)*3);
+        this.Vertices = [];
+        this.Normals = [];
+
+        //this.Index = 0;
+		
+		this.VIndex = 0;
+		this.NIndex = 0;
+		
+        this.generateSphere();
+     },
+	 
+     generateSphere: function() {
+        var v = [];
+		
+        v[0] = [0.0, 0.0, 1.0, 1.0];
+        v[1] = [0.0, 0.942809, -0.333333, 1.0];
+        v[2] = [-0.816497, -0.471405, -0.333333, 1.0];
+        v[3] = [0.816497, -0.471405, -0.333333, 1.0];
+
+        this.dividTriangle(v[0], v[1], v[2], 5);
+        this.dividTriangle(v[3], v[2], v[1], 5);
+        this.dividTriangle(v[0], v[3], v[1], 5);
+        this.dividTriangle(v[0], v[2], v[3], 5);
+    },
+
+    unit: function(p)
+    {
+        var c = vec4.create(); var d = 0.0;
+        for(var i=0; i<3; i++)
+            d += p[i]*p[i];
+        d = Math.sqrt(d);
+        if(d > 0.0)
+            for(var i=0; i<3; i++)
+                c[i] = p[i]/d;
+        c[3] = 1.0;
+        return c;
+    },
+
+    dividTriangle: function(a, b, c, n){
+        var v1; var v2; var v3;
+        if(n>0)
+        {
+            var temp = vec4.create();
+            temp = vec4.add(temp, a, b);
+            v1 = this.unit(temp);
+            temp = vec4.add(temp, a, c);
+            v2 = this.unit(temp);
+            temp = vec4.add(temp, b, c);
+            v3 = this.unit(temp);
+
+            this.dividTriangle(a , v1, v2, n-1);
+            this.dividTriangle(c , v2, v3, n-1);
+            this.dividTriangle(b , v3, v1, n-1);
+            this.dividTriangle(v1, v3, v2, n-1);
+        }
+        else{
+		/*
+            this.Vertices[this.Index] = a;
+            this.Normals[this.Index] = vec3.fromValues(a[0], a[1], a[2]);//get the normal of the triangle
+            this.Index++;
+            this.Vertices[this.Index] = b;
+            this.Normals[this.Index] = vec3.fromValues(b[0], b[1], b[2]);
+            this.Index++;
+            this.Vertices[this.Index] = c;
+            this.Normals[this.Index] = vec3.fromValues(c[0], c[1], c[2]);
+            this.Index++;
+			*/
+            this.Vertices[this.VIndex] = a[0];
+			this.VIndex++;
+            this.Vertices[this.VIndex] = a[1];
+			this.VIndex++;			
+            this.Vertices[this.VIndex] = a[2];
+			this.VIndex++;			
+            this.Vertices[this.VIndex] = a[3];
+			this.VIndex++;			
+            this.Normals[this.NIndex] = a[0];
+            this.NIndex++;
+            this.Normals[this.NIndex] = a[1];
+            this.NIndex++;
+            this.Normals[this.NIndex] = a[2];
+            this.NIndex++;			
+
+            this.Vertices[this.VIndex] = b[0];
+			this.VIndex++;
+            this.Vertices[this.VIndex] = b[1];
+			this.VIndex++;			
+            this.Vertices[this.VIndex] = b[2];
+			this.VIndex++;			
+            this.Vertices[this.VIndex] = b[3];
+			this.VIndex++;			
+            this.Normals[this.NIndex] = b[0];
+            this.NIndex++;
+            this.Normals[this.NIndex] = b[1];
+            this.NIndex++;
+            this.Normals[this.NIndex] = b[2];
+            this.NIndex++;
+			
+
+            this.Vertices[this.VIndex] = c[0];
+			this.VIndex++;
+            this.Vertices[this.VIndex] = c[1];
+			this.VIndex++;			
+            this.Vertices[this.VIndex] = c[2];
+			this.VIndex++;			
+            this.Vertices[this.VIndex] = c[3];
+			this.VIndex++;			
+            this.Normals[this.NIndex] = c[0];
+            this.NIndex++;
+            this.Normals[this.NIndex] = c[1];
+            this.NIndex++;
+            this.Normals[this.NIndex] = c[2];
+            this.NIndex++;
+			
+        }
+	}
+
+});
+		
