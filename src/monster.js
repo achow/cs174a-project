ee89be2f5616714b51f0f57175e299d64132ba40
Steven <steven@steven.dev.thevelozgroup.com>
2013-06-03 22:45:45 -0700

    changed ai for random monster

--- src/monster.js
+++ src/monster.js
@@ -16,6 +16,7 @@ def ("Monster") << Actor ({
         this.initial_position = this.position;
 		
 		this.selected = false;
+        this.checkforstart = false;
 		this.wait = 0;
     },
 	
@@ -70,47 +71,79 @@ def ("Monster") << Actor ({
 
 		else {
             
-            // Otherwise move in a random valid direction
-            var dirmove = Math.floor(Math.random() * 4);
-            for (var i = 0; i < 4; i++) {
-                switch (dirmove) {
-                    case 0: // North
-                        if (this.world.getMapElement(this.position.x, this.position.y-1) != MAPELEMENT.WALL
-                            && this.getDirection() != DIRECTION.DOWN) {
-                            this.setDirection(DIRECTION.DOWN);
+            if (this.checkforstart == false)
+            {
+                if (this.world.pacman.direction != DIRECTION.NONE)
+                {
+                    this.checkforstart = true;
+                    this.direction = this.world.pacman.direction;
+                }
+
+            }
+
+            switch (this.getDirection()) {
+                    case DIRECTION.DOWN: // Down
+                        if (this.world.getMapElement(this.position.x, this.position.y-1) == MAPELEMENT.WALL
+                            || this.world.getMapElement(this.position.x-1, this.position.y) != MAPELEMENT.WALL
+                            || this.world.getMapElement(this.position.x+1, this.position.y) != MAPELEMENT.WALL)
+                             {
+                            this.dirmove= Math.floor(Math.random() * 4);
+                            this.setDirection(this.dirmove);
                             this.move();
                             return;
                         }
+                        else
+                            {this.move();}
                         break;
 
-                    case 1: // East
-                        if (this.world.getMapElement(this.position.x+1, this.position.y) != MAPELEMENT.WALL
-                            && this.getDirection() != DIRECTION.LEFT) {
-                            this.setDirection(DIRECTION.LEFT);
+                    case DIRECTION.UP: // Up
+                        if (this.world.getMapElement(this.position.x, this.position.y+1) == MAPELEMENT.WALL
+                            || this.world.getMapElement(this.position.x-1, this.position.y) != MAPELEMENT.WALL
+                            || this.world.getMapElement(this.position.x+1, this.position.y) != MAPELEMENT.WALL    
+                            )
+                         {
+                            this.dirmove= Math.floor(Math.random() * 4);
+                            this.setDirection(this.dirmove);
                             this.move();
                             return;
                         }
+                        else
+                            {this.move();}
+                            
                         break;
 
-                    case 2: // South
-                        if (this.world.getMapElement(this.position.x, this.position.y+1) != MAPELEMENT.WALL
-                            && this.getDirection() != DIRECTION.UP) {
-                            this.setDirection(DIRECTION.UP);
+                    case DIRECTION.LEFT: // Left
+                        if (this.world.getMapElement(this.position.x-1, this.position.y) == MAPELEMENT.WALL
+                            || this.world.getMapElement(this.position.x, this.position.y+1) != MAPELEMENT.WALL
+                            || this.world.getMapElement(this.position.x, this.position.y-1) != MAPELEMENT.WALL)
+                            {
+                            this.dirmove= Math.floor(Math.random() * 4);
+                            this.setDirection(this.dirmove);
                             this.move();
                             return;
                         }
+                        else
+                        {
+                            this.move();
+                        }
                         break;
-                    case 3: // West
-                        if (this.world.getMapElement(this.position.x-1, this.position.y) != MAPELEMENT.WALL
-                            && this.getDirection() != DIRECTION.RIGHT) {
-                            this.setDirection(DIRECTION.RIGHT);
+                    case DIRECTION.RIGHT: // Right
+                        if (this.world.getMapElement(this.position.x+1, this.position.y) == MAPELEMENT.WALL
+                            || this.world.getMapElement(this.position.x, this.position.y+1) != MAPELEMENT.WALL
+                            || this.world.getMapElement(this.position.x, this.position.y-1) != MAPELEMENT.WALL
+                            )
+                            {
+                            this.dirmove= Math.floor(Math.random() * 4);
+                            this.setDirection(this.dirmove);
                             this.move();
                             return;
+                        }
+                        else
+                        {
+                            this.move();
                         }
                         break;
                 }
-                dirmove = (dirmove + 1) % 4;
-            }
 		}
     },
     
